1ab2f4292a1137f046caa9f9211081ab793262a7
Keshav Kumar <aboutkeshavkumar@hotmail.com>
2025-01-05 21:55:26 +0530

    define input types for AppOwner and AppAuth

--- packages/common/src/index.ts
+++ packages/common/src/index.ts
@@ -1,2 +1,20 @@
-export const app = 10;
-export const app2 = 102;
+import {z} from 'zod';
+
+export const appOwnerSignupInput = z.object({
+   email:z.string().email(),
+   password:z.string().min(8)
+})
+
+export const appOwnerSigninInput = z.object({
+   email:z.string().email(),
+   password:z.string().min(8)
+})
+
+export const appAuthSignupInput = z.object({
+   email:z.string().email(),
+   password:z.string().min(8)
+})
+
+export type AppOwnerSignupInput = z.infer<typeof appOwnerSignupInput>;
+export type AppOwnerSignInInput = z.infer<typeof appOwnerSigninInput>;
+export type AppAuthSignupInput = z.infer<typeof appAuthSignupInput>;
