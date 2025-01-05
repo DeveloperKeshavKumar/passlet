ecf101da3aa94407969d4270765f7a3eda98fe78
Keshav Kumar <aboutkeshavkumar@hotmail.com>
2025-01-05 21:57:54 +0530

    remove useless LOC

--- apps/backend/src/index.ts
+++ apps/backend/src/index.ts
@@ -1,13 +1,8 @@
 import { Hono } from 'hono'
-import { app2 } from '@common/index'
 import routes from './routes'
 
 const app = new Hono()
 
 app.route('/', routes);
-
-app.get('/app', (c) => {
-  return c.text(`${app2}`);
-})
 
 export default app
